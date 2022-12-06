2549c1cc6bf1a70edc5941c0411b4ecc2729b2ca
stupachek <alona.stupak@gmail.com>
2022-12-06 16:41:59 +0200

    eslint

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 'use strict';
-const express = require('express');
+import express from 'express';
 // Constants
 const HOST = '0.0.0.0';
-const PORT = 80;
+const PORT = 8080;
 const app = express();
 app.get('/', (req, res) => {
     res.send('Hello! I\'m trying!');
